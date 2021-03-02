10003fea3bb677b3d3442e3f9b79f705161ce9b6
elight <minghir@gmail.com>
2021-03-02 11:09:54 +0200

    Update int_ex1.c

--- C/int_ex1.c
+++ C/int_ex1.c
@@ -1,1 +1,20 @@
 
+//An Example of Getting a Key from Keyboard
+#include <stdio.h>
+#include <dos.h>
+int main()
+{
+
+	while(1){
+		//An Example of Getting a Key from KeyboardRemember to include dos.h in your file.
+		char x; int y; 		//declare x to be “char” type
+		_AH=0x0; 			//choose service 0x0 with _AH
+		geninterrupt(0x16); //invoke interrupt 0x16
+		x=_AL;//get ASCII code of key from _AL
+		printf("The key is: %c\n", x ); //print the key out
+		if(x == 27)break; // if escape (ASCII:27) exit
+	}
+	
+getch();
+return 0;
+}
