8c15f9d6cbd3689c26b843c2a8d8157baa40586a
elight <minghir@gmail.com>
2021-03-14 20:57:22 +0200

    Update lvga.h

--- pong/LVGA/lvga.h
+++ pong/LVGA/lvga.h
@@ -3,14 +3,49 @@
 #define VGA_256_COLOR_MODE  0x13      /* use to set 256-color mode. */
 #define TEXT_MODE           0x03      /* use to set 80x25 text mode. */
 
+#define SC_INDEX            0x03c4    /* VGA sequence controller */
+#define SC_DATA             0x03c5
+#define PALETTE_INDEX       0x03c8    /* VGA digital-to-analog converter */
+#define PALETTE_DATA        0x03c9
+#define GC_INDEX            0x03ce    /* VGA graphics controller */
+#define GC_DATA             0x03cf
+#define CRTC_INDEX          0x03d4    /* VGA CRT controller */
+#define CRTC_DATA           0x03d5
+#define INPUT_STATUS_1      0x03da
+
+#define MAP_MASK            0x02      /* Sequence controller registers */
+#define ALL_PLANES          0xff02
+#define MEMORY_MODE         0x04
+
+#define LATCHES_ON          0x0008    /* Graphics controller registers */
+#define LATCHES_OFF         0xff08
+
+#define HIGH_ADDRESS        0x0C      /* CRT controller registers */
+#define LOW_ADDRESS         0x0D
+#define UNDERLINE_LOCATION  0x14
+#define MODE_CONTROL        0x17
+
+#define DISPLAY_ENABLE      0x01      /* VGA input status bits */
+#define VRETRACE            0x08
+
+
 #define SCREEN_WIDTH        320       /* width in pixels of mode 0x13 */
 #define SCREEN_HEIGHT       200       /* height in pixels of mode 0x13 */
 #define NUM_COLORS          256       /* number of colors in mode 0x13 */
 
 #define sgn(x) ((x<0)?-1:((x>0)?1:0)) /* macro to return the sign of a
                                          number */
+										 
+#define aratio (int)( (double)SCREEN_WIDTH/(double)SCREEN_HEIGHT )
+
+#define BITMAP_WIDTH        32
+#define BITMAP_HEIGHT       25
+#define ANIMATION_FRAMES    24
+#define TOTAL_FRAMES        140
+
 typedef unsigned char  byte;
 typedef unsigned short word;
+typedef unsigned long  dword;
 typedef long fixed16_16;
 
 
@@ -23,13 +58,32 @@ typedef long fixed16_16;
 #define false 0
 typedef int bool;
 
+typedef struct tagBITMAP              /* the structure for a bitmap. */
+{
+  word width;
+  word height;
+  byte palette[256*3];
+  byte *data;
+} BITMAP;
+
+typedef struct tagOBJECT              /* the structure for a moving object
+                                         in 2d space; used for animation */
+{
+  int x,y;
+  int dx,dy;
+  byte width,height;
+} OBJECT;
+
 
 void initialize_graphics(bool dblbfr);
-
 void show_buffer();
 
-	
-
+void fskip(FILE *fp, int num_bytes);	
+void load_bmp(char *file,BITMAP *b);
+void draw_bitmap(BITMAP *bmp,int x,int y);
+void draw_transparent_bitmap(BITMAP *bmp,int x,int y);
+void wait(int ticks);
+void set_palette(byte *palette);
 										 
 /**************************************************************************
  *  set_mode                                                              *
@@ -60,6 +114,7 @@ void line_fast(int x1, int y1, int x2, int y2, byte color);
  **************************************************************************/
 
 void circle_fast(int x,int y, int radius, byte color);
+void circle_slow(int x,int y, int radius, byte color);
 
 /**************************************************************************
  *  circle_fill                                                           *
@@ -81,6 +136,10 @@ void rect_fast(int left,int top, int right, int bottom, byte color);
  **************************************************************************/
 
 void rect_fill(int left,int top, int right, int bottom, byte color);
+
+void put_ch(int x, int y, int c, int fc, int bc);
+void put_str(int x, int y, char *string, int fc, int bc);
+void Circle(int x, int y, int radius, unsigned char color);
 
 
 								 
