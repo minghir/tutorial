33edd058477b13b18eebc484af5e52878295586e
elight <minghir@gmail.com>
2021-03-19 10:33:46 +0200

    Update comp.bat

--- pong/comp.bat
+++ pong/comp.bat
@@ -4,4 +4,4 @@ del *.swp
 del dbg.txt
 cls
 REM  c:\app\tc\bin\tcc -Ic:\app\tc\include;c:\app\tc\src\pong\lvga -Lc:\app\tc\lib pong_vga.c lvga\keyb.c lvga\lvga.c
-c:\app\tc\bin\tcc -ml -Ic:\app\tc\include;c:\app\tc\src\pong\lvga -Lc:\app\tc\lib pongvga2.c lvga\keyb.c lvga\lvga.c
+c:\app\tc\bin\tcc -ml -Ic:\app\tc\include;c:\app\tc\src\pong\lvga -Lc:\app\tc\lib pongvga2.c lvga\keyb.c lvga\lvga.c lvga\cfg.c
