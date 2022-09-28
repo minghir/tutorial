035702913a0fc8121554e81e56e11a9eb2d3cbd7
elight <minghir@gmail.com>
2022-09-28 14:40:07 +0300

    Update test1.glsl

--- shaders/test1.glsl
+++ shaders/test1.glsl
@@ -12,3 +12,17 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ){
 void mainImage( out vec4 fragColor, in vec2 fragCoord ){
     fragColor = vec4((fragCoord-.5*iResolution.xy)/iResolution.y+sin(fragCoord.xy)*cos(fragCoord.yx),0.,1.);
 }
+/*
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
+    // Time varying pixel color
+  //  vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+  vec3 col  = vec3(
+        step(0.,sin(fragCoord.x/25.)))*step(0.,vec3(sin(fragCoord.y/25.))
+        );
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
+*/
