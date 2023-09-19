81825c044c96bc51d80f3c75692e2c0c039780ac
elight <minghir@gmail.com>
2023-09-19 14:42:01 +0300

    Create halftweet.glsl

---
+++ shaders/halftweet.glsl
@@ -0,0 +1,10 @@
+void mainImage( out vec4 o, in vec2 i )
+{
+//o*=.0;o[int(i)/2&int(i.y)/2]++;
+//o*=.0;o[int(i)/3&int(i.y)/33]++;
+//o*=.0;o[int(i)&int(i.y)/33]++;
+//o*=.0;o[int(i)/33&int(i.y)*9]++;
+//o*=.0;o[int(i)&int(i.y)]+=8.;
+//o*=.7;o[int(i)&int(i.y)]+=9.;
+o*=.0;o[(int(i)&int(i.y))/26]++;
+}
